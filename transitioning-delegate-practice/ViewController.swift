5af8da037359e446910d86fd8fa16572e02850c1
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-06-08 11:57:43 +0900

    Initial Commit

---
+++ transitioning-delegate-practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  transitioning-delegate-practice
+//
+//  Created by SaitoKeisei on 2017/06/08.
+//  Copyright © 2017 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
